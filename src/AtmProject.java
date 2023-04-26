027e8080364679b3482d4a25dd2287aaaf3065c6
ismail <keskin9756@hotmail.com>
2023-04-26 22:00:30 +0300

    v2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -2,6 +2,8 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("Bu Bir ATM Projesidir");
         //version1
+        System.out.println("Java is easy");
+        //version2
 
     }
 
